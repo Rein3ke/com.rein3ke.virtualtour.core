6e0943997b501645d4b740363111bb4c9ab13571
Marvin Kullick <mkullick@web.de>
2022-02-01 19:35:06 +0100

    Initial commit;

---
+++ Runtime/ExhibitData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ExhibitData", menuName = "Exhibits/Create ExhibitData", order = 1)]
+public class ExhibitData : ScriptableObject
+{
+    public string exhibitName;
+    [TextArea]
+    public string exhibitDescription;
+
+    public AudioClip[] audioClips;
+    public Texture2D[] images;
+}
